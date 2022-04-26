ae16b0c0288907c86552599c7d858becbf6ac17c
Chris-N <sdchrisn@gmail.com>
2022-04-26 13:30:14 -0700

    Added fire particle to ball when hit. Still wip for particle to follow ball

--- Assets/Scripts/BallCollisionController.cs
+++ Assets/Scripts/BallCollisionController.cs
@@ -6,6 +6,8 @@ public class BallCollisionController : MonoBehaviour
 {
     GameManager gm;
     [SerializeField] ParticleSystem collisionParticle;
+    [SerializeField] ParticleSystem trailParticle;
+    bool isTrailParticleOn;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,12 @@ public class BallCollisionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ClearResource();
+
+        if (isTrailParticleOn)
+        {
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,10 +31,25 @@ public class BallCollisionController : MonoBehaviour
         if (collision.gameObject.CompareTag("Bat"))
         {
             Debug.Log("HIT! - Audio: boom");
-            ParticleSystem ps = Instantiate(collisionParticle);
-            ps.transform.position = gameObject.transform.position;
+            collisionParticle.transform.position = gameObject.transform.position;
+            Instantiate(collisionParticle);
             collisionParticle.Play();
+
+            trailParticle.transform.position = gameObject.transform.position;
+            Instantiate(trailParticle);
+            trailParticle.Play();
+            isTrailParticleOn = true;
+
             gm.IncrementBallHitCount();
         }
+    }
+    private void ClearResource()
+    {
+        if(gameObject.transform.position.y < 0)
+        {
+            isTrailParticleOn = false;
+            Destroy(gameObject);
+        }
+
     }
 }
